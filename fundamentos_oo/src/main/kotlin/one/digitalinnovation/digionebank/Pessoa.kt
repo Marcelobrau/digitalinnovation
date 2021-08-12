56f3f771fb97a7175ff3244b7acb7f372061a104
marcelobrau <mbraulino@gmail.com>
2021-08-12 17:19:54 -0300

    alteração classe

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,14 +1,18 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa(
-    val nome: String = "jether",
+class Pessoa{
+    val nome: String = "jether"
     val cpf: String  = "12.123.123-11"
-)
+
+    constructor()
+
+    fun pesssoainfo() = "$nome e $cpf"
+}
 
 fun main(){
     val jether =  Pessoa()
 
-    println(jether.nome)
-    println(jether.cpf)
+    println(jether.pesssoainfo())
+
 }
 
