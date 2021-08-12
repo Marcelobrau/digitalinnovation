7ea082533ace5d4bcb468792c93ce1fd0dd9cbfd
marcelobrau <mbraulino@gmail.com>
2021-08-12 15:56:45 -0300

    commit classe Pessoa

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,7 +1,14 @@
 package one.digitalinnovation.digionebank
 
-abstract class Pessoa(
-    val nome: String,
-    val cpf: String
+class Pessoa(
+    val nome: String = "jether",
+    val cpf: String  = "12.123.123-11"
 )
+
+fun main(){
+    val jether =  Pessoa()
+
+    println(jether.nome)
+    println(jether.cpf)
+}
 
